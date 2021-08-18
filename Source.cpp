bb77c1e13795322c932202d28caaa43ce81d57ae
User <User@LAPTOP-GRAR24N3>
2021-08-18 15:34:06 +0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include "stdio.h"
+int main() {
+	int a, b, c, max;
+	scanf_s("%d %d %d", &a, &b, &c);
+	if (a >= b && a >= c) {
+		max = a;
+	}
+	if (b >= a && b >= c) {
+		max = b;
+	}
+	if (c >= a && c >= b) {
+		max = c;
+	}
+	printf("%d", max);
+}
